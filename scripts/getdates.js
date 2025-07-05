09b4b6c19e07a1af72ef632d2972e5bb622e8e4d
ievbosaru <ievbosaru@byupathway.edu>
2025-07-05 16:02:36 -0700

    week 1

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Dynamically set current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Dynamically set last modified date
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
